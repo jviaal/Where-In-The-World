42efea1a7b673c171b4071fbd973a60ee774a973
jviaal <gegabalen@gmail.com>
2020-06-15 09:42:19 +0800

    Updates

---
+++ precache-manifest.97a0672fab6d4605a8f7228f45b0f53e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5601d4cd53eb84d15a57956ecb8b0453",
+    "url": "/Where-In-The-World/index.html"
+  },
+  {
+    "revision": "e7a952df91074f7c6d40",
+    "url": "/Where-In-The-World/static/css/main.bbfccead.chunk.css"
+  },
+  {
+    "revision": "1b7f588477cf666630fb",
+    "url": "/Where-In-The-World/static/js/2.518c9c32.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Where-In-The-World/static/js/2.518c9c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a952df91074f7c6d40",
+    "url": "/Where-In-The-World/static/js/main.34e19900.chunk.js"
+  },
+  {
+    "revision": "3833ac9fe37d0412c3f8",
+    "url": "/Where-In-The-World/static/js/runtime-main.5fdfe377.js"
+  }
+]);
